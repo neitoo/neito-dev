64c903649af99b0ace797aeb98b60ea3bc44d1ff
neitoo <neito.dev@gmail.com>
2024-10-29 13:54:56 +0500

    ➕ Modified footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,10 @@
 const Footer = () => {
     return (
-        <></>
+        <footer className="footer__container">
+            <section className="content__container">
+                <p>© 2024 neitoo</p>
+            </section>
+        </footer>
     );
 }
 
