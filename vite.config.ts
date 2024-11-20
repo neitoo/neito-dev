93e223e24dbf65010d9f93df9850100a0e868212
neitoo <neito.dev@gmail.com>
2024-11-20 16:02:59 +0500

    🛠fix alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import {fileURLToPath} from "url";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@assets': path.resolve(__dirname, 'src/assets'),
-      '@images': path.resolve(__dirname, 'src/assets/images'),
-      '@hooks': path.resolve(__dirname, 'src/hooks')
-    },
+    alias: [
+      { find: '@components', replacement: fileURLToPath(new URL('./src/components', import.meta.url)) },
+      { find: '@assets', replacement: fileURLToPath(new URL('./src/assets', import.meta.url)) },
+      { find: '@images', replacement: fileURLToPath(new URL('./src/assets/images', import.meta.url)) },
+      { find: '@hooks', replacement: fileURLToPath(new URL('./src/hooks', import.meta.url)) },
+      { find: '@interfaces', replacement: fileURLToPath(new URL('./src/interfaces', import.meta.url)) },
+      { find: '@api', replacement: fileURLToPath(new URL('./src/api', import.meta.url)) },
+    ],
   },
 });
