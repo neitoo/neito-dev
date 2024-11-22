f4b4d32a58ff3f55d162153ba3022b4d3b4a9c66
neitoo <neito.dev@gmail.com>
2024-11-22 16:33:07 +0500

    🛠Added filling of the detailed project page (test)

--- src/features/project/ProjectDetails.tsx
+++ src/features/project/ProjectDetails.tsx
@@ -1,6 +1,26 @@
+import { useProjects } from "@hooks/useProjects";
+import { useParams } from "react-router-dom";
+
 const ProjectDetails = () => {
+    const { id } = useParams<{id: string}>();
+    const { projects, isLoading } = useProjects();
+
+    if (isLoading) {
+        return <div>Загрузка проектов...</div>
+    }
+
+    const project = projects.find((project) => project.id === id);
+
+    if (!project) {
+        return <div>Проект не найден</div>
+    }
+
     return (
-        <></>
+        <main className="main__container">
+            <div>
+                <h1>{project.name}</h1>
+            </div>
+        </main>
     );
 }
 
