07415c7dff05e1f84c617e17691b18a69be9e990
neitoo <neito.dev@gmail.com>
2024-11-29 16:30:26 +0500

    🖌Redesigned the main page design

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,136 +1,179 @@
-import { CopyText } from '@components/copyText';
-import scan from '@images/interface-essential-iris-scan.svg';
+import { CopyText } from "@components/copyText";
+import helloLogo from "@images/5006-mibored.png";
 
-import coding from '@images/coding-apps-websites-programming-hold-code.svg';
-import tools from '@images/interface-essential-hammer-1.svg';
-import module from '@images/coding-apps-websites-module.svg';
-import addition from '@images/coding-apps-websites-plugin.svg';
+import coding from "@images/code.svg";
+import tools from "@images/tools.svg";
+import addition from "@images/all.svg";
 
-import telegram from '@images/telegram.svg';
-import mail from '@images/mail.svg';
+import telegram from "@images/telegram.svg";
+import mail from "@images/mail.svg";
+import good from "@images/good-menhera.gif";
 
-import { useRef } from 'react';
-
+import { useRef } from "react";
+import { Stars } from "@components/stars";
+import { useIntersection } from "@hooks/useIntersection";
 
 const Home = () => {
-    const sectionRef = useRef<HTMLDivElement>(null);
-    const handleScroll = () => {
-        sectionRef.current?.scrollIntoView({ behavior: 'smooth' });
-    };
+  const sectionRef = useRef<HTMLDivElement>(null);
+  const handleScroll = () => {
+    sectionRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
 
-    return (
-        <main className="main__container">
-            <section className="content__container">
-                <h1 className="text-2xl 2xl:text-5xl uppercase">
-                    neito | dev
-                </h1>
-            </section>
+  const skillsRef1 = useRef<HTMLUListElement>(null);
+  const skillsRef2 = useRef<HTMLUListElement>(null);
+  const skillsRef3 = useRef<HTMLUListElement>(null);
 
-            <section className="content__container">
-                <div className="flex gap-2 flex-nowrap">
-                    <p className="text-3xl">
-                        Привет!
-                    </p>
-                    <img src={scan} className="select-none size-9 animate-appearance"/>
-                </div>
-            </section>
+  const isVisible1 = useIntersection(skillsRef1, '100px');
+  const isVisible2 = useIntersection(skillsRef2, '100px');
+  const isVisible3 = useIntersection(skillsRef3, '100px');
 
-            <section className="content__container">
-                <div className="flex flex-col items-center gap-16 flex-nowrap py-14">
-                    <p className="text-xl text-center">
-                        Моё имя <span className="bg-red-700 px-2">Никита</span>. Занимаюсь разработкой сайтов и мобильных приложений. <br/>Тут я делюсь своими проектами, которые могут вас заинтересовать.
-                    </p>
-                </div>
-            </section>
+  return (
+    <main className="main__container">
+      <section className="content__container h-screen pt-32">
+        <div className="flex flex-col w-full h-full">
+          <div className="flex gap-2 flex-nowrap">
+            <img
+              src={helloLogo}
+              className="select-none size-9 animate-appearance"
+            />
+            <p className="text-2xl md:text-3xl">Привет!</p>
+          </div>
 
-            <section className="content__container">
-                <div onClick={handleScroll} className="flex items-center justify-around w-fit cursor-pointer">
-                    <div className="w-12 h-20 border-2 border-dashed border-white relative mouse-before"></div>
-                </div>
-            </section>
+          <div className="flex flex-col items-start gap-12 flex-nowrap py-14">
+            <div>
+              <p className="text-xl leading-[2]">Меня зовут </p>
+              <p className="font-bold text-4xl leading-[1.5] md:leading-[1] md:text-5xl xl:leading-[1.5] xl:text-6xl">
+                Никита.
+              </p>
+              <p className="font-bold text-4xl leading-[2] md:leading-[1.5] md:text-5xl xl:leading-[2] xl:text-6xl">
+                Увлеченный разработчик.
+              </p>
+            </div>
+            <p className="text-xl text-left">
+              Занимаюсь разработкой сайтов и мобильных приложений. <br />
+              Тут я делюсь своими проектами, которые могут вас заинтересовать.
+            </p>
+          </div>
 
-            <section ref={sectionRef} className="content__container mt-80">
-                <p className="text-3xl mb-12">Технологии в которых я разбираюсь</p>
-                <ul className="flex flex-col gap-12 w-full">
-                    <li className="list__skills">
-                        <div className='skill__image-block'>
-                            <img className='size-12' src={coding} alt="" />
-                        </div>
-                        
-                        <ul className="skills__names">
-                            <li>JavaScript</li>
-                            <li>TypeScript</li>
-                            <li>PHP</li>
-                            <li>HTML/CSS</li>
-                            <li>Kotlin</li>
-                        </ul>
-                    </li>
-                    <li className="list__skills">
-                        <div className='skill__image-block'>
-                            <img className='size-12' src={tools} alt="" />
-                        </div>
-                        
-                        <ul className="skills__names">
-                            <li>Git/GitHub</li>
-                            <li>REST API</li>
-                            <li>Vite</li>
-                            <li>Electron.js</li>
-                            <li>Node.js</li>
-                        </ul>
-                    </li>
-                    <li className="list__skills">
-                        <div className='skill__image-block'>
-                            <img className='size-12' src={module} alt="" />
-                        </div>
-                        
-                        <ul className="skills__names">
-                            <li>React</li>
-                            <li>Tailwind CSS</li>
-                            <li>October CMS</li>
-                            <li>Jetpack Compose</li>
-                        </ul>
-                    </li>
-                    <li className="list__skills">
-                        <div className='skill__image-block'>
-                            <img className='size-12' src={addition} alt="" />
-                        </div>
-                        
-                        <ul className="skills__names">
-                            <li>Photoshop, Figma</li>
-                            <li>Linux</li>
-                        </ul>
-                    </li>
-                </ul>
-            </section>
+          <a
+            href="https://tyumen.hh.ru/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="relative bg-red-600 w-fit p-2 rounded-md transition-colors duration-200 hover:bg-white hover:text-black group"
+          >
+            Нанять меня
+            <div className="absolute bottom-full left-0 hidden group-hover:block group-active:block">
+              <img src={good} alt="" className="size-32" />
+            </div>
+          </a>
 
-            <section className="content__container">
-                <p className="text-3xl mb-12">Контакты</p>
+          <div
+            onClick={handleScroll}
+            className="flex items-center justify-around w-full cursor-pointer mt-12"
+          >
+            <div className="w-12 h-20 border-2 border-solid rounded-md border-white/50 relative mouse-before"></div>
+          </div>
+        </div>
+        <Stars />
+      </section>
 
-                <p className="text-xl text-center">
-                Не стесняйтесь писать мне, если у вас есть вопросы или предложения. Я всегда открыт для общения и новых идей. 
-                </p>
+      <div className="border-t border-white/10 w-full"></div>
 
-                <p className='text-xl text-center mt-10'>
-                    Связаться со мной можно так:
-                </p>
+      <section ref={sectionRef} className="content__container pt-32 pb-16">
+        <p className="text-3xl mb-12">Технологии в которых я разбираюсь</p>
+        <ul className="flex flex-col gap-12 w-full">
+          <li className="list__skills">
+            <div className="skill__image-block">
+              <img className="size-8" src={coding} alt="" />
+              <strong>Программирование и языки разметки</strong>
+            </div>
 
-                <div className='flex flex-row mt-5 w-full justify-center'>
-                    <div className='flex flex-col gap-5 items-left'>
-                        <div className='flex flex-row gap-3 items-center'>
-                            <a href="https://t.me/neitoo" title='Открыть телеграм аккаунт'>
-                                <img src={telegram} alt="Telegram" />
-                            </a>
-                            <CopyText text="@neitoo" />
-                        </div>
-                        <div className='flex flex-row gap-3 items-center'>
-                            <img src={mail} alt="Email" />
-                            <CopyText text="neito.dev@gmail.com"/>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </main>
-    );
-}
+            <ul ref={skillsRef1} className="skills__names">
+              {['JavaScript', 'TypeScript', 'PHP', 'SQL', 'HTML/CSS(SASS)', 'Kotlin'].map((skill, index) => (
+                <li
+                  key={skill}
+                  style={{
+                    animationDelay: `${index * 100}ms`
+                  }}
+                  className={`${isVisible1 ? 'animate-fadeUp' : ''}`}
+                >
+                  {skill}
+                </li>
+              ))}
+            </ul>
+          </li>
+          <li className="list__skills">
+            <div className="skill__image-block">
+              <img className="size-8" src={tools} alt="" />
+              <strong>Фреймворки, библиотеки и инструменты разработки</strong>
+            </div>
+
+            <ul ref={skillsRef2} className="skills__names">
+              {['React', 'Tailwind CSS', 'Node.js', 'Electron.js', 'Jetpack Compose', 'Vite', 'October CMS'].map((skill, index) => (
+                <li
+                  key={skill}
+                  style={{
+                    animationDelay: `${index * 100}ms`
+                  }}
+                  className={`${isVisible2 ? 'animate-fadeUp' : ''}`}
+                >
+                  {skill}
+                </li>
+              ))}
+            </ul>
+          </li>
+          <li className="list__skills">
+            <div className="skill__image-block">
+              <img className="size-8" src={addition} alt="" />
+              <strong>Инструменты и доп. навыки</strong>
+            </div>
+
+            <ul ref={skillsRef3} className="skills__names">
+              {['Git/GitHub', 'Linux', 'Photoshop, Figma', 'Android Studio'].map((skill, index) => (
+                <li
+                  key={skill}
+                  style={{
+                    animationDelay: `${index * 100}ms`
+                  }}
+                  className={`${isVisible3 ? 'animate-fadeUp' : ''}`}
+                >
+                  {skill}
+                </li>
+              ))}
+            </ul>
+          </li>
+        </ul>
+      </section>
+      
+      <div className="border-t border-white/10 w-full"></div>
+
+      <section className="content__container pt-32">
+        <p className="text-3xl mb-12">Контакты</p>
+
+        <p className="text-xl">
+          Не стесняйтесь писать мне, если у вас есть вопросы или предложения. Я
+          всегда открыт для общения и новых идей.
+        </p>
+
+        <p className="text-xl mt-10">Связаться со мной можно так:</p>
+
+        <div className="flex flex-row mt-5 w-full">
+          <div className="flex flex-col gap-5 items-left">
+            <div className="flex flex-row gap-3 items-center">
+              <a href="https://t.me/neitoo" title="Открыть телеграм аккаунт">
+                <img src={telegram} alt="Telegram" />
+              </a>
+              <CopyText text="@neitoo" />
+            </div>
+            <div className="flex flex-row gap-3 items-center">
+              <img src={mail} alt="Email" />
+              <CopyText text="neito.dev@gmail.com" />
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+};
 
 export default Home;
