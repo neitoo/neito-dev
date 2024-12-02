1fa85a9652c53b21b396e1cb8a08c1e748e661e4
neitoo <neito.dev@gmail.com>
2024-12-02 16:14:50 +0500

    ➕Added styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,7 +22,8 @@ export default {
         starSmall: 'animStar 12s linear infinite',
         starMedium: 'animStar 25s linear infinite',
         starBig: 'animStar  38s linear infinite',
-        fadeUp: 'fadeUp 0.5s ease-out forwards',
+        fade: 'fade 0.5s ease-out forwards',
+        fadeDown: 'fadeDown 0.5s ease-out forwards',
       },
       keyframes: {
         spin360twice: {
@@ -47,14 +48,22 @@ export default {
           '94%' : { opacity: '0'},
           '100%' : { transform: 'translateY(-500px)'},
         },
-        fadeUp: {
+        fade: {
+          'from': {
+            opacity: '0'
+          },
+          'to': {
+            opacity: '1'
+          }
+        },
+        fadeDown: {
           'from': {
             opacity: '0',
-            transform: 'translateY(20px)'
+            transform: 'translateY(-20px)'
           },
           'to': {
             opacity: '1',
-            transform: 'translateY(0)'
+            transform: 'translateY(0px)'
           }
         },
       },
@@ -73,7 +82,7 @@ export default {
     },
     container: {
       screens: {
-        '2xl': '1024px'
+        '2xl': '900px'
       }
     }
   },
