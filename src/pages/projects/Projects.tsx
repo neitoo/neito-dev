a39b473a361622b43e30b6fe835cf91d2262fd82
neitoo <neito.dev@gmail.com>
2024-12-02 16:19:24 +0500

    🛠Added project page styling

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -5,10 +5,17 @@ const Projects = () => {
     return (
         <main className="main__container">
             <section className="content__container pt-32">
-                <h1 className="text-2xl 2xl:text-5xl uppercase">проекты</h1>
+                <h1 className="text-2xl 2xl:text-5xl uppercase w-full text-center animate-fadeDown">Проекты</h1>
+
+                <p className="pt-16 animate-fade">
+                    Это проекты, которые я сделал, некоторые из них личные проекты или работа, или для других ситуаций.
+                    Если вы хотите увидеть абсолютно все мои проекты, перейдите на мой <a href="https://github.com/neitoo" target="_blank" rel="noopener noreferrer" className="hover:underline">github.com/neitoo</a>.
+                </p>
             </section>
             
-            <ProjectsList/>
+            <section className="content__container pt-16">
+                <ProjectsList/>
+            </section>
         </main>
 
     );
